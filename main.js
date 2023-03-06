9f3c5ee8585717c4038ddde5367eada201c04b23
yusef270110 <105560183+yusef270110@users.noreply.github.com>
2023-03-06 17:36:43 -0500

    Add files via upload
    todo pero al reves

---
+++ main.js
@@ -0,0 +1,7 @@
+function tnt(){
+    rotienerret=document.getElementById("ognodnom").value 
+    ollop=document.getElementById("ehc").value 
+    localStorage.setItem("ognodnom",rotienerret)
+    localStorage.setItem("ehc",ollop)
+    window.location="peru.html"
+};
